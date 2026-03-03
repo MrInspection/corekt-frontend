eb5b76ec0380e7f9cb3a4318ec8164dc5ad2ca02
Moussa <moussaabdallahhoumadi@gmail.com>
2026-03-03 13:10:58 +0100

    feat: add route pages

---
+++ src/app/(dashboard)/projects/[projectId]/version/[version]/generate/page.tsx
@@ -0,0 +1,5 @@
+import { CreateVersionReportView } from "@/features/projects/views/reports/create-version-report.view";
+
+export default function GenerateReportPage() {
+  return <CreateVersionReportView />;
+}
