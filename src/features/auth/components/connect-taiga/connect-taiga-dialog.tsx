65e9445c04faafc44b0da51d63150b86af540237
Moussa <moussaabdallahhoumadi@gmail.com>
2026-03-08 16:13:59 +0100

    refactor: add form field component to simply form creatio

--- src/features/auth/components/connect-taiga/connect-taiga-dialog.tsx
+++ src/features/auth/components/connect-taiga/connect-taiga-dialog.tsx
@@ -19,7 +19,8 @@ import {
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { PasswordInput } from "@/components/ui/password-input";
-import { LoginSchema } from "@/features/auth/validation/auth.schema";
+import { LoginFormSchema } from "@/features/auth/validation/auth.schema";
+import { FormField } from "@/features/shared/form/form-field";
 import { Icons } from "@/features/shared/ui/icons";
 
 export function ConnectTaigaDialog() {
@@ -29,7 +30,7 @@ export function ConnectTaigaDialog() {
       password: "",
     },
     validators: {
-      onSubmit: LoginSchema,
+      onSubmit: LoginFormSchema,
     },
     onSubmit: async ({ value }) => {
       alert(JSON.stringify(value));
@@ -67,14 +68,10 @@ export function ConnectTaigaDialog() {
             }}
           >
             <FieldGroup className="gap-4">
-              <form.Field
-                name="email"
-                children={(field) => {
-                  const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid;
-                  return (
-                    <Field data-invalid={isInvalid} className="grid gap-1.5">
-                      <FieldLabel htmlFor={field.name}>Email</FieldLabel>
+              <form.Field name="email">
+                {(field) => (
+                  <FormField field={field} label="Email">
+                    {(isInvalid) => (
                       <Input
                         id={field.name}
                         name={field.name}
@@ -82,22 +79,16 @@ export function ConnectTaigaDialog() {
                         onBlur={field.handleBlur}
                         onChange={(e) => field.handleChange(e.target.value)}
                         aria-invalid={isInvalid}
+                        placeholder="name@example.com"
                       />
-                      {isInvalid && (
-                        <FieldError errors={field.state.meta.errors} />
-                      )}
-                    </Field>
-                  );
-                }}
-              />
-              <form.Field
-                name="password"
-                children={(field) => {
-                  const isInvalid =
-                    field.state.meta.isTouched && !field.state.meta.isValid;
-                  return (
-                    <Field data-invalid={isInvalid} className="grid gap-1.5">
-                      <FieldLabel htmlFor={field.name}>Password</FieldLabel>
+                    )}
+                  </FormField>
+                )}
+              </form.Field>
+              <form.Field name="password">
+                {(field) => (
+                  <FormField field={field} label="Password">
+                    {(isInvalid) => (
                       <PasswordInput
                         id={field.name}
                         name={field.name}
@@ -105,14 +96,12 @@ export function ConnectTaigaDialog() {
                         onBlur={field.handleBlur}
                         onChange={(e) => field.handleChange(e.target.value)}
                         aria-invalid={isInvalid}
+                        placeholder="●●●●●●●●●●●●"
                       />
-                      {isInvalid && (
-                        <FieldError errors={field.state.meta.errors} />
-                      )}
-                    </Field>
-                  );
-                }}
-              />
+                    )}
+                  </FormField>
+                )}
+              </form.Field>
             </FieldGroup>
           </form>
         </div>
